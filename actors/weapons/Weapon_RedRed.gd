0e5fdf836aa0c4688ade9a851cc55bf336aa88d8
rockgem <63050183+rockgem@users.noreply.github.com>
2023-05-23 17:54:28 +0800

    gun rotate ok

--- actors/weapons/Weapon_RedRed.gd
+++ actors/weapons/Weapon_RedRed.gd
@@ -5,10 +5,10 @@ extends Node2D
 func _physics_process(delta):
 	var dir = global_position.direction_to(get_global_mouse_position())
 	if dir.x < -0.5:
-		get_node('%Sprite2D').flip_h = true
-		get_node('%Sprite2D2').flip_h = true
+		get_node('%Sprite2D').flip_v = true
+		get_node('%Sprite2D2').flip_v = true
 	else:
-		get_node('%Sprite2D').flip_h = false
-		get_node('%Sprite2D2').flip_h = false
+		get_node('%Sprite2D').flip_v = false
+		get_node('%Sprite2D2').flip_v = false
 	
 	$Node2D.look_at(get_global_mouse_position())
