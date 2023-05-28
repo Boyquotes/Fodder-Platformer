944508d2ff3ce4e6f09c139d9f23c4f43097ee09
rockgem <63050183+rockgem@users.noreply.github.com>
2023-05-28 08:09:55 +0800

    add craft? label while holding 2 materials on hand

--- scenes/UI.gd
+++ scenes/UI.gd
@@ -4,3 +4,10 @@ extends Control
 func _ready():
 	ManagerGame.global_mat_slots.append($MaterialSlot)
 	ManagerGame.global_mat_slots.append($MaterialSlot2)
+
+
+func _physics_process(delta):
+	if ManagerGame.material_on_hand[0] != '' and ManagerGame.material_on_hand[1] != '':
+		$CraftLabel.show()
+	else:
+		$CraftLabel.hide()
